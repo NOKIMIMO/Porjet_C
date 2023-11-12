d83c53d060b1ae2839369725b0ef076b0d01fbf1
Romain <romainguiffant49@gmail.com>
2023-11-12 21:32:55 +0100

    bug fix map player

--- game.c
+++ game.c
@@ -45,14 +45,15 @@ int game(int save_flag){
             return 0;
         } else if (ret == 0) {
             //Exit normal du joueur / fin de combat
-            write_player(player);
             cpt++;
             map = initMap(7, 7, x_depart - 1, y_depart - 1);
+            player->map = map;
             int *temp2 = searchEntry(map, 7, 7);
             x_depart = temp2[0] + 1;
             y_depart = temp2[1] + 1;
             set_pos_x_P(player, x_depart);
             set_pos_y_P(player, y_depart);
+            write_player(player);
         } else if (ret == 2) {
             //Exit complete du jeu
             return 2;
